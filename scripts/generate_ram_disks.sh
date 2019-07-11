8272cf69b89b746f0b50266d0c4022229137c44d
mydriasis <samuel.vidovich@gmail.com>
2019-07-10 23:00:15 -0400

    wrong order!

--- scripts/generate_ram_disks.sh
+++ scripts/generate_ram_disks.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 for i in {0..249}; do
-    echo "1:$i ram$i"
+    echo "ram$i 1:$i"
 done
-echo "1:250 initrd"
+echo "initrd 1:250"
--- scripts/generate_ttys.sh
+++ scripts/generate_ttys.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 for i in {1..255}; do
     if (( $i < 64 )); then
-        echo "4:$i tty$i"
+        echo "tty$i 4:$i"
     else
-        echo "4:$((255-$i+64)) ttyS$((255-$i))"
+        echo "ttyS$((255-$i)) 4:$((255-$i+64))"
     fi
 done
